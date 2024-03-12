2f60d82b9b39e2dd1ff85c7ee73442c99c89e5e2
Pradeep Kumar R <153484703+PradeepDuo@users.noreply.github.com>
2024-03-12 21:48:38 +0530

    swagger done for both controllers

--- src/main/java/com/ff/Stock_Management_App/config/AppConfig.java
+++ src/main/java/com/ff/Stock_Management_App/config/AppConfig.java
@@ -17,19 +17,19 @@ public class AppConfig {
 	public OpenAPI usersMicroserviceOpenAPI() {
 
 		Server localhost = new Server();
-		localhost.setUrl("http://localhost:8081");
+		localhost.setUrl("http://localhost:8080");
 		localhost.setDescription("Development environment");
 
 		Contact contact = new Contact();
 		contact.setEmail("fireflink@mail.in");
-		contact.setName("WorkSpace Management System");
-		contact.setUrl("https://wms.in");
+		contact.setName("Stock Management System");
+		contact.setUrl("https://sms.in");
 
 		License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
 
-		Info info = new Info().title("WMS App RESTful Web Service documentation").version("1.0").contact(contact)
+		Info info = new Info().title("SMS App RESTful Web Service documentation").version("1.0").contact(contact)
 				.description("This API exposes endpoints to manage Application.")
-				.termsOfService("https://wms.in/terms").license(mitLicense);
+				.termsOfService("https://sms.in/terms").license(mitLicense);
 
 		return new OpenAPI().info(info).servers(List.of(localhost));
 	}
